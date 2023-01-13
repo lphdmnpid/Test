26bf4885ce12799adccb4c5caaef2c9cf42a382b
Maxim <maxim.belov05@inbox.ru>
2023-01-13 17:49:32 +0300

    Добавляем декоратор.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+def two(a):
+    #Декоратор к one
+    def dec(*args,**kwargs):
+        ans=0
+        for i in range(3):
+            ans+=a(*args,**kwargs)
+            print(i+1)
+            print(ans)
+    return dec
+
 def one(*args,**kwargs):
+    #Считает сумму квадратов
     ans=0
     for i in args:
         ans+=i**2
@@ -7,3 +18,6 @@ def one(*args,**kwargs):
     return ans
 
 print(one(1,2,3,6,one=5,two=3))
+
+three=two(one)
+three(1,2,3,6,one=5,two=3)
