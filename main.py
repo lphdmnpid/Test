ed16aa8032e99a08c56ae6d8c719cd926fd1a267
Maxim <maxim.belov05@inbox.ru>
2023-01-13 17:38:30 +0300

    Изменение main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+def one(*args,**kwargs):
+    ans=0
+    for i in args:
+        ans+=i**2
+    for i in kwargs.items():
+        ans+=i[1]**2
+    return ans
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(one(1,2,3,6,one=5,two=3))
