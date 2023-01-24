45e0b1f6e94a11fcd7a3ae1c84cefccc117bf808
Maxim <maxim.belov05@inbox.ru>
2023-01-24 18:00:16 +0300

    Заканчиваем тест

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 def two(a):
     #Декоратор к one
     def dec(*args,**kwargs):
@@ -33,3 +34,76 @@ print(one(1,2,3,6,one=5,two=3))
 three=two(one)
 three(1,2,3,6,one=5,two=3)
 one("four")
+'''
+class Item:
+    def __init__(self,name,end,priority):
+        self.__name=name
+        self.__end=end
+        self.__priority=priority
+
+    def Change(self,**kwargs):
+        for n,i in kwargs.items():
+            if n=="name":
+                self.__name=i
+            if n=="end":
+                self.__end=i
+            if n=="priority":
+                self.__priority=i
+    
+    @property
+    def Deal(self):
+        print(f"Название: {self.__name}")
+        print(f"Сделать до {self.__end}")
+        print(f"Приоритет: {self.__priority}")
+
+class ItemList:
+    def __init__(self):
+        self.deals=[]
+    
+    def add(self,*args):
+        if len(args)==3:
+            self.deals.append(Item(*args))
+        else:
+            print("Нужно ровно 3 аргумента.")
+    
+    def GetAll(self):
+        for i in self.deals:
+            i.Deal
+    
+    def GetDate(self,date):
+        for i in self.deals:
+            if i.__end==date:
+                i.Deal
+    
+    def GetPriority(self,priority):
+        for i in self.deals:
+            if i.__priority<=priority:
+                i.Deal
+
+    def dl(self,index):
+        try:
+            self.deals.pop(int(index))
+        except:
+            print("Неверный ввод.")
+    
+    def Change(self,index,**kwargs):
+        try:
+            self.deals[int(index)].Change(**kwargs)
+        except:
+            print("Неверный ввод.")
+
+a=Item("Олимпиада","15.01.23","10")
+a.Deal
+al=ItemList()
+al.add("Олимпиада","15.01.23","10")
+al.GetAll()
+
+
+
+
+
+
+
+
+
+
